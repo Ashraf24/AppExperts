8587f52ad11f0f051192788fdd9c12d865fe4eb6
Ashraful Alam <alam24@usa.com>
2020-09-17 19:47:31 +0100

    latest files

---
+++ Printbio.java
@@ -0,0 +1,14 @@
+
+public class Printbio {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 
+	       System.out.println("Hello " + "There " + 
+	         "This is more text " +
+	         "And even more text");
+	
+	}
+
+}
